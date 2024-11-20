eb8c9174a32a6594169e97b2bca9e501e8d8f4a3
Kenny Paredes <jkparedes3@utpl.edu.ec>
2024-11-20 12:38:35 -0500

    EJEMPLO051COEMNTARIO

--- ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo051.java
+++ ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo051.java
@@ -45,7 +45,8 @@ public class Ejemplo051 {
         
         promedio_final = suma_total/limite;
         // System.out.printf("El promedio final es %f\n", promedio_final);
-        mensajeFinal = String.format("%s\n%f", 
+        mensajeFinal = String.format("%s\n%f", //String.format ayuda a manejar el formato 
+                // y es muy parecido al printf
                 mensajeFinal, promedio_final);
         
         System.out.printf("%s\n", mensajeFinal);
